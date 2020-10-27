7b6ab4079a08ef2230e3131986eb5a4f7f4f1309
Ricardo Sousa <ti.ricardo.sousa@gmail.com>
2020-10-27 19:25:24 -0300

    Add toolbar.

--- app/src/main/java/br/com/ricardo/mysubscribers/ui/MainActivity.kt
+++ app/src/main/java/br/com/ricardo/mysubscribers/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package br.com.ricardo.mysubscribers.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import br.com.ricardo.mysubscribers.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        main_toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
